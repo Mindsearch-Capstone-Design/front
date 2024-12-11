26e68cc0b68c3e1364afea6e51f6be4eaa64f1ee
MinHyekok <dlalsgur7711@naver.com>
2024-12-11 13:30:48 +0900

    modify rank count

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -4,11 +4,11 @@ import "./Rank.css";
 const Rank = ({ selectedSentiment, keywords }) => {
   const getBackgroundColor = () => {
     if (selectedSentiment === "긍정") {
-      return "rgb(186, 227, 255)";
+      return "rgb(198, 232, 255)";
     } else if (selectedSentiment === "중립") {
-      return "rgb(164, 255, 144)";
+      return "rgb(182, 255, 166)";
     } else if (selectedSentiment === "부정") {
-      return "rgb(255, 231, 243)";
+      return "rgb(255, 223, 240)";
     }
     return "transparent";
   };
@@ -22,7 +22,9 @@ const Rank = ({ selectedSentiment, keywords }) => {
             className="rank-word"
             style={{ backgroundColor: getBackgroundColor() }}
           >
-            {index + 1}위: {keyword.word} ({keyword.count}회)
+            <span className="rank-index">{index + 1}위:</span>{" "}
+            <span className="rank-keyword">{keyword.word}</span>
+            <span className="rank-count">{keyword.count}회</span>
           </li>
         ))}
       </ul>
