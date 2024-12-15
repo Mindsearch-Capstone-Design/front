e3f16fc4542d68bd3a94676727a817f52f803f6c
MinHyekok <dlalsgur7711@naver.com>
2024-12-15 20:58:31 +0900

    delete unnecessary dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ import Comments from "./components/Comments";
 import "./App.css";
 
 function App() {
-  const [lineGraphData, setLineGraphData] = useState([]);
-  const [selectedEmotion, setSelectedEmotion] = useState("긍정");
   const [hasData, setHasData] = useState(false);
   const [activeButton, setActiveButton] = useState(0);
 
@@ -47,49 +45,14 @@ function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleSearch = (startDate, endDate) => {
+  const handleSearch = () => {
     setHasData(true);
     setActiveButton(0); // Default active button
-
-    const positive = [10, 20, 15, 25];
-    const neutral = [15, 10, 20, 30];
-    const negative = [5, 10, 5, 10];
-
-    const start = startDate ? new Date(startDate) : new Date("2024-01-01");
-    const end = endDate ? new Date(endDate) : new Date("2024-12-01");
-    const interval = Math.floor((end - start) / 3);
-
-    const period = Array.from({ length: 4 }, (_, i) => {
-      const date = new Date(start.getTime() + interval * i);
-      return date.toISOString().split("T")[0];
-    });
-
-    const data = [
-      {
-        id: "긍정",
-        color: "#0000FF",
-        data: period.map((p, index) => ({ x: p, y: positive[index] })),
-      },
-      {
-        id: "중립",
-        color: "#008000",
-        data: period.map((p, index) => ({ x: p, y: neutral[index] })),
-      },
-      {
-        id: "부정",
-        color: "#FF0000",
-        data: period.map((p, index) => ({ x: p, y: negative[index] })),
-      },
-    ];
-
-    setLineGraphData(data);
   };
 
   return (
     <div className="App">
-      <Header
-        onSearch={(startDate, endDate) => handleSearch(startDate, endDate)}
-      />
+      <Header onSearch={handleSearch} />
       <SideMenu
         activeButton={activeButton}
         setActiveButton={setActiveButton}
@@ -103,18 +66,14 @@ function App() {
       ) : (
         <div className="grid-container">
           <div className="wordcloud-rank" ref={wordCloudRef}>
-            <WordCloudRank
-              selectedEmotion={selectedEmotion}
-              setSelectedEmotion={setSelectedEmotion}
-              hasData={hasData}
-            />
+            <WordCloudRank hasData={hasData} />
           </div>
           <div className="charts-container" ref={chartRef}>
             <div className="pie-chart">
               <PieChart hasData={hasData} />
             </div>
             <div className="line-graph">
-              <LineGraph data={lineGraphData} />
+              <LineGraph />
             </div>
           </div>
           <div className="comments-section" ref={commentsRef}>
